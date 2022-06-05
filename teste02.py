a4713219abcb35963a9f6921983b0874bd1e1e61
HMatias <105978315+HenriqueBMatias@users.noreply.github.com>
2022-06-05 16:55:14 -0300

    ADD nova linha na linha 5

--- teste02.py
+++ teste02.py
@@ -2,8 +2,10 @@ print('hello word')
 <<<<<<< HEAD
 print("Linha 2")
 print("Henrique")
+print("LInha semlinha")
 =======
 pint ("ed073910dea2facbe7513cdb16b957d87a75326b")
+
 
 valor=10
 if valor <= 10:
